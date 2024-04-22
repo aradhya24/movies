54f41b7e3f25649f9285fd37dfc354fcf70e6972
Aradhya Chandrakant Dange <88555012+aradhya24@users.noreply.github.com>
2024-04-23 02:59:22 +0530

    Update axiosConfig.js

--- movie-gold-v1/src/api/axiosConfig.js
+++ movie-gold-v1/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://13.50.93.72:8080',
+    baseURL:'http://16.171.38.17:8080',
     headers:{"ngrok-skip-browser-warning":"true"}
 });
